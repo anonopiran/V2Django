bc0c4b5580d2ac0d751fa5bf760112aa623e7198
anonopiran <anonopiran@proton.me>
2022-11-01 17:26:52 +0330

    added postgres support

--- V2Django/settings.py
+++ V2Django/settings.py
@@ -88,10 +88,9 @@ WSGI_APPLICATION = "V2Django.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.db_url(
+        "DATABASE_DSN", default=f"sqlite:///{BASE_DIR}/storage/sqlite.db"
+    )
 }
 
 # Password validation
