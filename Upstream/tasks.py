06ef09e0d2aec62ab7c49cac0082309eb11b63c3
anonopiran <anonopiran@proton.me>
2023-05-12 16:26:53 +0330

    bug fix
    add/rm user task retries

--- Upstream/tasks.py
+++ Upstream/tasks.py
@@ -4,18 +4,32 @@ from celery import shared_task
 from celery.utils.log import get_task_logger
 from Upstream.manager import UserMan
 from yarl import URL
-from requests.exceptions import ConnectionError
+from requests.exceptions import ConnectionError, HTTPError
 
 logger = get_task_logger(__name__)
 
 
-@shared_task(autoretry_for=(ConnectionError,), max_retries=10, retry_backoff=5)
+@shared_task(
+    autoretry_for=(
+        ConnectionError,
+        HTTPError,
+    ),
+    max_retries=10,
+    retry_backoff=5,
+)
 def add_user(endpoint: URL, uuid: UUID, email):
     UserMan(endpoint=endpoint).user__add(uuid=uuid, email=email)
     logger.info(f"user added {endpoint}-{email}")
 
 
-@shared_task(autoretry_for=(ConnectionError,), max_retries=10, retry_backoff=5)
+@shared_task(
+    autoretry_for=(
+        ConnectionError,
+        HTTPError,
+    ),
+    max_retries=10,
+    retry_backoff=5,
+)
 def rm_user(endpoint: URL, email):
     UserMan(endpoint=endpoint).user__rm(email=email)
     logger.info(f"user removed {endpoint}-{email}")
